ce929dc30715ab9f5c1eb8439318f6d0f25d52e6
Oliver Lorenz <lorenz@cosmocode.de>
2011-01-07 18:44:07 +0100

    fixed JacksonTranscoder, license for it

--- src/main/java/de/cosmocode/palava/cache/JacksonTranscoder.java
+++ src/main/java/de/cosmocode/palava/cache/JacksonTranscoder.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2010 CosmoCode GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.cosmocode.palava.cache;
 
 import net.spy.memcached.CachedData;
@@ -17,7 +33,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 /**
- * <p></p>
+ * <p>
+ * Jackson based transcoder that uses the {@link MappingJsonFactory} to transcode
+ * any POJO.
+ * </p>
  * <p>
  * Created on: 07.01.11
  * </p>
@@ -61,15 +80,12 @@ public enum JacksonTranscoder implements Transcoder<Object> {
         final DataInputStream classIn = new DataInputStream(in);
         try {
             final Class<?> valueType = Class.forName(classIn.readUTF());
-            valueType.getConstructor().setAccessible(true);
             LOG.trace("Read class {}", valueType);
             final JsonParser parser = factory.createJsonParser(in);
             return parser.readValueAs(valueType);
         } catch (IOException e) {
             throw new IllegalStateException(e);
         } catch (ClassNotFoundException e) {
-            throw new IllegalStateException(e);
-        } catch (NoSuchMethodException e) {
             throw new IllegalStateException(e);
         }
     }
