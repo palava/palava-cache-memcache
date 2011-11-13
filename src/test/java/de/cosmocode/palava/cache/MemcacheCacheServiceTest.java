2790f5e9ecb980d3f7224322c7f787df4e8c8bb7
Oliver Lorenz <oliver-lorenz@gmx.de>
2011-11-13 22:29:05 +0100

    Fixed test, need newest palava-memcache for that

--- src/test/java/de/cosmocode/palava/cache/MemcacheCacheServiceTest.java
+++ src/test/java/de/cosmocode/palava/cache/MemcacheCacheServiceTest.java
@@ -16,17 +16,21 @@
 
 package de.cosmocode.palava.cache;
 
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
 import com.google.inject.name.Names;
-
 import de.cosmocode.palava.core.DefaultRegistryModule;
+import de.cosmocode.palava.core.Framework;
+import de.cosmocode.palava.core.Palava;
+import de.cosmocode.palava.core.lifecycle.LifecycleException;
 import de.cosmocode.palava.core.lifecycle.LifecycleModule;
-import de.cosmocode.palava.ipc.ConnectionAwareUnitOfWorkScopeModule;
-import de.cosmocode.palava.ipc.IpcScopeModule;
 import de.cosmocode.palava.memcache.MemcacheClientModule;
+import de.cosmocode.palava.memcache.MemcacheLocalServerModule;
+import de.cosmocode.palava.scope.SingletonUnitOfWorkScopeModule;
+import org.junit.After;
+import org.junit.Before;
 
 /**
  * Tests the {@link MemcacheCacheService}.
@@ -34,7 +38,43 @@ import de.cosmocode.palava.memcache.MemcacheClientModule;
  * @author Oliver Lorenz
  */
 public final class MemcacheCacheServiceTest extends CacheServiceTest {
-    
+
+    private final Framework framework = Palava.newFramework(new AbstractModule() {
+
+        @Override
+        protected void configure() {
+            install(new LifecycleModule());
+            install(new DefaultRegistryModule());
+            install(new SingletonUnitOfWorkScopeModule());
+
+            final String address = "127.0.0.1";
+            final int port = 11211;
+
+            // configure local JVM memcache server
+            bindConstant().annotatedWith(Names.named("local.memcache.server.verbose")).to(true);
+            bindConstant().annotatedWith(Names.named("local.memcache.server.address")).to(address);
+            bindConstant().annotatedWith(Names.named("local.memcache.server.port")).to(port);
+            install(new MemcacheLocalServerModule());
+
+            // configure memcache client
+            bindConstant().annotatedWith(Names.named("memcache.addresses")).to(address + ":" + port);
+            install(new MemcacheClientModule());
+
+            install(new MemcacheCacheServiceModule());
+        }
+
+    }, new Properties());
+
+    @Before
+    public void start() throws LifecycleException {
+        framework.start();
+    }
+
+    @After
+    public void stop() throws LifecycleException {
+        framework.stop();
+    }
+
     @Override
     protected long lifeTime() {
         return 2;
@@ -62,23 +102,7 @@ public final class MemcacheCacheServiceTest extends CacheServiceTest {
 
     @Override
     public CacheService unit() {
-        final CacheService unit = Guice.createInjector(
-                new LifecycleModule(),
-                new DefaultRegistryModule(),
-                new IpcScopeModule(),
-                new ConnectionAwareUnitOfWorkScopeModule(),
-                new AbstractModule() {
-                    
-                    @Override
-                    protected void configure() {
-                        bindConstant().annotatedWith(Names.named("memcache.addresses")).to("192.168.0.12:11211");
-                    }
-                    
-                },
-                new MemcacheClientModule(),
-                new MemcacheCacheServiceModule()).
-            getInstance(CacheService.class);
-        return unit;
+        return framework.getInstance(CacheService.class);
     }
 
 }
